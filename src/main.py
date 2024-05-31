237dcd8d365a4e5461bb7cf2695ce30914045e99
Allmyfriendsaredead <96424117+extahsi@users.noreply.github.com>
2024-05-31 00:31:49 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+from gui import MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
