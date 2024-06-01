8d28af878033501848f18afa7cc6b58a6136e599
Allmyfriendsaredead <96424117+extahsi@users.noreply.github.com>
2024-06-01 10:07:55 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
+from PyQt5 import QtWidgets
 import sys
-import logging
-from PyQt5.QtWidgets import QApplication
 from gui import MainWindow
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    logger.info("Application started")
-    app = QApplication(sys.argv)
+def main():
+    app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
-    logger.info("Application exited")
+
+if __name__ == '__main__':
+    main()
